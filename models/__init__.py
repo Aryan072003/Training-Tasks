6051c73497ca4e09410a6255919449a17e4033f3
Aryan072003 <129711307+Aryan072003@users.noreply.github.com>
2025-03-08 12:58:29 +0530

    Delete models directory

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import cafe_order
-from . import product_template
-from . import cafe_customer
-from . import cafe_order_line
--- models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, models, fields
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    customer_id = fields.Many2one('cafe.customer', string='Customer')
-
