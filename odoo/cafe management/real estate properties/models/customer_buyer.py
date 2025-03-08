017ce25470806fe7222f2fca96cbdc44676072bb
Aryan072003 <129711307+Aryan072003@users.noreply.github.com>
2025-03-08 13:06:15 +0530

    Delete odoo/cafe management/real estate properties directory

--- odoo/cafe management/real estate properties/models/customer_buyer.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-class PropertyCustomer(models.Model):
-    _inherit = 'res.users'
-    _description = 'Property Customers'
-
-class PropertyBuyer(models.Model):
-    _inherit = 'res.partner'
-    _description = 'Property Buyers'
